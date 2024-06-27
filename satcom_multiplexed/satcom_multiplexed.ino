e9ec9e68f53df639286aef871874dd228b83189d
holographic_sol <88397088+holographicSol@users.noreply.github.com>
2024-06-27 13:34:22 +0100

    Update satcom_multiplexed.ino

--- satcom_multiplexed/satcom_multiplexed.ino
+++ satcom_multiplexed/satcom_multiplexed.ino
@@ -226,8 +226,8 @@ struct SatDatatruct {
   double abs_latitude_gngga_0  = 0.0; // absolute latitude
   double abs_longitude_gngga_0 = 0.0; // absolute longditude
 
-  double abs_latitude_gnrmc_0  = 0.0; // type double absolute latitude
-  double abs_longitude_gnrmc_0 = 0.0; // type double absolute longditude
+  double abs_latitude_gnrmc_0  = 0.0; // absolute latitude
+  double abs_longitude_gnrmc_0 = 0.0; // absolute longditude
 
   double latitude_meter        = 0.0000901; // one meter converted to latitude
   double longitude_meter       = 0.0000899; // one meter converted to longitude
