8db6113c4aac42f98ebdbbdb11cb14a7d39533ae
holographic_sol <88397088+holographicSol@users.noreply.github.com>
2024-06-28 02:09:11 +0100

    Update satcom_multiplexed.ino

--- satcom_multiplexed/satcom_multiplexed.ino
+++ satcom_multiplexed/satcom_multiplexed.ino
@@ -4,7 +4,7 @@
 
 
                                      Receives and Processes Transmissions from Satellites
-                        A global location Sensor & Time Sensor. For projects requiring location data and or time.
+    A global location Sensor & Time Sensor. For projects requiring location data and or time to be positionally aware and syncronized.
          Tested with WTGPS300 (WITMOTION) Satellite module this project receives data from satellites, sorts that data into
      variables that can be used later, and creates new $ sentences containing new data, calculated from the data received.
 
