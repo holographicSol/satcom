0e38847463f70a36a64d22494971107cd5c95268
holographic_sol <88397088+holographicSol@users.noreply.github.com>
2024-07-01 09:35:55 +0100

    Update satcom_multiplexed.ino

--- satcom_multiplexed/satcom_multiplexed.ino
+++ satcom_multiplexed/satcom_multiplexed.ino
@@ -1412,7 +1412,8 @@ bool static_flag_gpatt_equal(int Ri, int Fi) {
 
 bool run_state_flag_gpatt_equal(int Ri, int Fi) {
   Serial.println("[CONNECTED] run_state_flag_gpatt_equal");
-   
+  if (atoi(gpattData.run_state_flag) == relayData.relays_data[Ri][Fi][2]) {return true;}
+  else {return false;}
 }
 
 // ----------------------------------------------------------------------------------------------------------------------------
@@ -1908,6 +1909,9 @@ void systems_Check() {
 
         // put true or false in the temporary matrix
         else if (strcmp(relayData.relays[Ri][Fi], relayData.ins_gpatt_equal) == 0) {tmp_matrix[0][Fi] = ins_gpatt_equal(Ri, Fi);}
+
+        // put true or false in the temporary matrix
+        else if (strcmp(relayData.relays[Ri][Fi], relayData.run_state_flag_gpatt_equal) == 0) {tmp_matrix[0][Fi] = run_state_flag_gpatt_equal(Ri, Fi);}
 
         // Serial.println("[tmp_matrix] " + String(Fi) + " [DAT] " + String(tmp_matrix[0][Fi]));
       }
