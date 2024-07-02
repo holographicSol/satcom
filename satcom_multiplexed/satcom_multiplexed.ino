218507510d9a6e53b48045bfd23506060846d2a8
holographic_sol <88397088+holographicSol@users.noreply.github.com>
2024-07-02 02:22:53 +0100

    Update satcom_multiplexed.ino

--- satcom_multiplexed/satcom_multiplexed.ino
+++ satcom_multiplexed/satcom_multiplexed.ino
@@ -1510,10 +1510,7 @@ void systems_Check() {
         else if (strcmp(relayData.relays[Ri][Fi], relayData.fix_angle_flag_gpatt_in_range) == 0) {tmp_matrix[0][Fi] = check_in_range(gpattData.fix_angle_flag, Ri, Fi);}
 
         /*
-        the above checks are the 'basic' checks. with that out the way we can now build more 'advanced' checks/calculations from the basic data which may be something like using
-        datetime and coordinates to calculate sunrise for example, among infinite things we can calculate with all this data and then check for. checks should be as simple/light
-        as possible so that this main system can try to run as fast as it can, while the systems being turned on/off may also have access to the sentence dumps for any tasks
-        those systems require that information for.
+        the above checks are the 'basic' checks. with that out the way, we can now build more 'advanced' checks/calculations, using the same format.
         */
 
         // Serial.println("[tmp_matrix] " + String(Fi) + " [DAT] " + String(tmp_matrix[0][Fi]));
