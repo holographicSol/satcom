28c49bb506d77965afd1efc973fa626fc8aea387
holographic_sol <88397088+holographicSol@users.noreply.github.com>
2024-07-06 17:33:54 +0100

    Update satcom_multiplexed.ino

--- satcom_multiplexed/satcom_multiplexed.ino
+++ satcom_multiplexed/satcom_multiplexed.ino
@@ -2143,7 +2143,7 @@ struct RelayStruct {
   Y: necessary if comparing to X.
   Z: necessary if comparing to X/Y in range.  
                 
-                0                                30
+                0                                       30
           0     1     2     
           X     Y     Z    
   {  {   0.0,  0.0,  0.0   }       {0}          }
