35b419d9500380b51b5fe056aad3065d2dcd280e
holographic_sol <88397088+holographicSol@users.noreply.github.com>
2024-07-10 16:18:06 +0100

    Update satcom_multiplexed.ino
    simplified time on time off.

--- satcom_multiplexed/satcom_multiplexed.ino
+++ satcom_multiplexed/satcom_multiplexed.ino
@@ -3147,33 +3147,14 @@ bool check_bool_false(bool _bool) {
   if (_bool == false) {return true;} else {return false;}
 }
 
-bool time_period_gngga_seconds_in_range(int n0, int n1) {
-  // n0 and n1 should both be <= 59.
+bool time_period_on_off(int n0, int n1, char * utc, int idx0, int idx1) {
+  // n0 and n1 should be < utc units max: seconds 59, hours 24 etc.
   // n0: seconds time on
   // n1: seconds time off
-  char sc[10];
-  sc[0] = gnggaData.utc_time[4];
-  sc[1] = gnggaData.utc_time[5];
-  if ((atoi(sc) >= n0) && (atoi(sc) <= n1)) {return true;} else {return false;}
-}
-
-bool time_period_gngga_minutes_in_range(int n0, int n1) {
-  // n0 and n1 should both be <= 59.
-  // n0: seconds time on
-  // n1: seconds time off
-  char sc[10];
-  sc[0] = gnggaData.utc_time[2];
-  sc[1] = gnggaData.utc_time[3];
-  if ((atoi(sc) >= n0) && (atoi(sc) <= n1)) {return true;} else {return false;}
-}
-
-bool time_period_gngga_hours_in_range(int n0, int n1) {
-  // n0 and n1 should both be <= 24.
-  // n0: seconds time on
-  // n1: seconds time off
-  char sc[10];
-  sc[0] = gnggaData.utc_time[0];
-  sc[1] = gnggaData.utc_time[1];
+  char sc[4];
+  sc[0] = utc[idx0];
+  sc[1] = utc[idx1];
+  Serial.println("" + String(sc) + " >= " + String (n0) + " && " + String(sc) + " <= " + String (n1));
   if ((atoi(sc) >= n0) && (atoi(sc) <= n1)) {return true;} else {return false;}
 }
 
@@ -3218,9 +3199,9 @@ void matrixSwitch() {
         // ----------------------------------------------------------------------------------------------------------------------------
         //                                                                                                       SYSTEMS CHECKS: SATCOM
 
-        else if (strcmp(relayData.relays[Ri][Fi], relayData.time_period_gngga_seconds_in_range) == 0) {tmp_matrix[Fi] = time_period_gngga_seconds_in_range(relayData.relays_data[Ri][Fi][0], relayData.relays_data[Ri][Fi][1]);}
-        else if (strcmp(relayData.relays[Ri][Fi], relayData.time_period_gngga_minutes_in_range) == 0) {tmp_matrix[Fi] = time_period_gngga_minutes_in_range(relayData.relays_data[Ri][Fi][0], relayData.relays_data[Ri][Fi][1]);}
-        else if (strcmp(relayData.relays[Ri][Fi], relayData.time_period_gngga_hours_in_range) == 0) {tmp_matrix[Fi] = time_period_gngga_hours_in_range(relayData.relays_data[Ri][Fi][0], relayData.relays_data[Ri][Fi][1]);}
+        else if (strcmp(relayData.relays[Ri][Fi], relayData.time_period_gngga_seconds_in_range) == 0) {tmp_matrix[Fi] = time_period_on_off(relayData.relays_data[Ri][Fi][0], relayData.relays_data[Ri][Fi][1], gnggaData.utc_time, 4, 5);}
+        else if (strcmp(relayData.relays[Ri][Fi], relayData.time_period_gngga_minutes_in_range) == 0) {tmp_matrix[Fi] = time_period_on_off(relayData.relays_data[Ri][Fi][0], relayData.relays_data[Ri][Fi][1], gnggaData.utc_time, 2, 3);}
+        else if (strcmp(relayData.relays[Ri][Fi], relayData.time_period_gngga_hours_in_range) == 0) {tmp_matrix[Fi] = time_period_on_off(relayData.relays_data[Ri][Fi][0], relayData.relays_data[Ri][Fi][1], gnggaData.utc_time, 0, 1);}
 
         // SATCOM: GNGGA
         else if (strcmp(relayData.relays[Ri][Fi], relayData.latitude_satcom_gngga_over) == 0) {tmp_matrix[Fi] = check_over_true(satData.location_latitude_gngga, relayData.relays_data[Ri][Fi][0]);}
