4bb07f145f750e3e30e52c79337b57683a2f3963
holographic_sol <88397088+holographicSol@users.noreply.github.com>
2024-07-14 01:46:51 +0100

    Update satcom_multiplexed.ino

--- satcom_multiplexed/satcom_multiplexed.ino
+++ satcom_multiplexed/satcom_multiplexed.ino
@@ -3768,10 +3768,9 @@ void SSD_Display_GPATT() {
   display_4.setTextAlignment(TEXT_ALIGN_LEFT); display_4.setColor(WHITE); display_4.drawString(4, 38, "Y " + String(gpattData.yaw));
   display_4.setTextAlignment(TEXT_ALIGN_RIGHT); display_4.setColor(WHITE); display_4.drawString(display_4.getWidth()-4, 18, String("RSF " + String(gpattData.run_state_flag)));
   display_4.setTextAlignment(TEXT_ALIGN_RIGHT); display_4.setColor(WHITE); display_4.drawString(display_4.getWidth()-4, 28, "GST " + String(gpattData.gst_data));
-  display_4.setTextAlignment(TEXT_ALIGN_RIGHT); display_4.setColor(WHITE); display_4.drawString(display_4.getWidth()-4, 38, "RIF " + String(gpattData.run_inetial_flag));
+  display_4.setTextAlignment(TEXT_ALIGN_RIGHT); display_4.setColor(WHITE); display_4.drawString(display_4.getWidth()-4, 38, "M " + String(gpattData.mileage));
   display_4.setTextAlignment(TEXT_ALIGN_LEFT); display_4.setColor(WHITE); display_4.drawString(4, 48, "INS " + String(gpattData.ins));
-  display_4.setTextAlignment(TEXT_ALIGN_CENTER); display_4.setColor(BLACK); display_4.drawString(display_4.getWidth()/2, 48, "M " + String(gpattData.mileage));
-  display_4.setTextAlignment(TEXT_ALIGN_RIGHT); display_4.setColor(WHITE); display_4.drawString(display_4.getWidth()-4, 48, "SF " + String(gpattData.static_flag) + " LF " + String(gpattData.line_flag));
+  display_4.setTextAlignment(TEXT_ALIGN_RIGHT); display_4.setColor(WHITE); display_4.drawString(display_4.getWidth()-4, 48, "RIF " + String(gpattData.run_inetial_flag)+ "SF " + String(gpattData.static_flag) + " LF " + String(gpattData.line_flag));
   display_4.display();
 }
 
