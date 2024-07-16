8486cffda3e0af62bad6f2dd4a64ba1e97e43e52
holographic_sol <88397088+holographicSol@users.noreply.github.com>
2024-07-16 08:57:01 +0100

    Update satcom_multiplexed.ino

--- satcom_multiplexed/satcom_multiplexed.ino
+++ satcom_multiplexed/satcom_multiplexed.ino
@@ -29,15 +29,16 @@
 
 
                         Ultimately this system is being built as a unit to turn on/off multiplexed relays (40+)
-                   where potentially anything can be plugged in such as simple modules or pre-programmed MCU's, 
-              making a foundation for other creative projects that may make use of such satellite and or inertial data.
-              The idea is that each relay is controlled by a compound of logic (limited by memory), and the logic itself
-              is programmable before and after flashing. Allowing for a reusable and general purpose system for any future
-              projects requiring such data. more advanced calculations are intended, such as emphemeris, astronomical etc.
+                     where potentially anything can be plugged in such as simple modules or pre-programmed MCU's, 
+               making a foundation for other creative projects that may make use of such satellite and or inertial data.
+               The idea is that each relay is controlled by a compound of logic (limited by memory), and the logic itself
+               is programmable before and after flashing. Allowing for a reusable and general purpose system for any future
+               projects requiring such data. more advanced calculations are intended, such as emphemeris, astronomical etc. now
+               that the foundational data has been handled.
 
-              The relays are currently simulated while the system is being built and the simulation is designed to be easily
-              relaceable by the actual relays themselves once the logic has been completed to a satisfactory degree and currently
-              all logic required to activate and deactivate relays is in place. 
+               The relays are currently simulated while the system is being built and the simulation is designed to be easily
+               relaceable by the actual relays themselves once the logic has been completed to a satisfactory degree and currently
+               all logic required to activate and deactivate relays is in place. 
 */
 
 // ----------------------------------------------------------------------------------------------------------------------------
